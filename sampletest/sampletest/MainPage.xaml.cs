bfef0167742143b1e30736927206c0fc7e34cff7
Alluri Mahesh <maheshasztech@gmail.com>
2019-04-19 17:32:28 +0530

    finished

---
+++ sampletest/sampletest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sampletest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void NextPage_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushModalAsync(new FirstPage());
+        }
+    }
+}
